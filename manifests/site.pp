c2bf63bffcbd2b551d38fbe9d7805225cbb2f8ff
Osama Younis <31683424+osamayy@users.noreply.github.com>
2020-05-23 19:18:32 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,3 @@
 node default {
-  file{'/root/README':
-  ensure  => file,
-  content => 'This is a readme file',
-  owner   => 'root',
-  }
-  file{'/root/README':
-    owner => 'root',
-  }
+  
 }
